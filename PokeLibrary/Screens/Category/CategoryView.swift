7e17c557ff680ea55df28cc349500e4d3f9eec58
Shubroto Shuvo <51680632+shuvofifty@users.noreply.github.com>
2022-09-12 12:28:01 -0400

    feat: Category View Added

--- PokeLibrary/Screens/Category/CategoryView.swift
+++ PokeLibrary/Screens/Category/CategoryView.swift
@@ -6,15 +6,33 @@
 //
 
 import Foundation
+import QGrid
 import SwiftUI
 
 struct CategoryView: View {
-    @ObservedObject var viewModel: ViewModel
+    @StateObject var viewModel: ViewModel
+    
+    var body: some View {
+        QGrid(viewModel.pokemonTypes, columns: 2) {type in
+            ZStack {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.orange)
+                    .frame(height: 100)
+                Text(type.name.capitalized)
+                    .bold()
+                    .foregroundColor(.white)
+            }
+        }
+        .onAppear {
+            viewModel.getPokemonTypes()
+        }
+    }
+}
+
+struct CategoryBoxCellView: View {
+    private let category: String
     
     var body: some View {
         Text("Category")
-            .onAppear {
-                viewModel.getPokemonTypes()
-            }
     }
 }
